6220b360027059e6fb1441734c063d88c319feaa
Awalhossain <waqin21@gmail.com>
2024-11-13 01:41:15 +0600

    fix: update JWT secret reference in auth middleware for token verification

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import httpStatus from 'http-status/dist';
+import httpStatus from 'http-status';
 import { Secret } from 'jsonwebtoken';
 import config from '../../config';
 import ApiError from '../../errors/ApiError';
@@ -17,7 +17,7 @@ const auth =
       // verify token
       let verifiedUser = null;
 
-      verifiedUser = jwtHelpers.verifyToken(token, config.jwt.secret as Secret);
+      verifiedUser = jwtHelpers.verifyToken(token, config.JWT_SECRET as Secret);
 
       req.user = verifiedUser; // role  , userid
 

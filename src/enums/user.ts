d143042cd87a38dd476936e7f1b180b2160d0498
Awalhossain <waqin21@gmail.com>
2024-11-16 19:26:54 +0600

    feat: update user role enum values to uppercase for consistency

--- src/enums/user.ts
+++ src/enums/user.ts
@@ -1,9 +1,9 @@
 /* eslint-disable no-unused-vars */
 export enum ENUM_USER_ROLE {
-  SUPER_ADMIN = 'super_admin',
-  ADMIN = 'admin',
-  MANAGER = 'manager',
-  CUSTOMER = 'customer',
-  RIDER = 'rider',
-  DELIVERY_BOY = 'delivery_boy',
+  SUPER_ADMIN = 'SUPER_ADMIN',
+  ADMIN = 'ADMIN',
+  MANAGER = 'MANAGER',
+  CUSTOMER = 'CUSTOMER',
+  RIDER = 'RIDER',
+  DELIVERY_BOY = 'DELIVERY_BOY',
 }

ca27d5a337a7daf54c76323890c89122f8f9a561
Awalhossain <waqin21@gmail.com>
2024-11-16 19:25:55 +0600

    feat: enhance food and category routes with authentication and new endpoints

--- src/app/api/v1/food/routes/food.routes.ts
+++ src/app/api/v1/food/routes/food.routes.ts
@@ -21,18 +21,35 @@ const addonController = new AddonController();
 
 router.post(
   '/create',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.MANAGER, ENUM_USER_ROLE.SUPER_ADMIN),
   upload.single('image'),
   foodController.createFood
 );
 
-router.patch('/update/:id', foodController.updateFoodDetails);
+router.patch('/update/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.MANAGER, ENUM_USER_ROLE.SUPER_ADMIN),
+  foodController.updateFoodDetails
+);
 
 router.get('/all', foodController.getAllFoods);
+router.get('/main-category/:id', foodController.getFoodByMainCategoryId);
+router.get('/sub-category/:id', foodController.getFoodBySubCategoryId);
 
+
+// category routes
 router.post('/category/create', categoryController.createCategory);
 router.get('/category/all', categoryController.getAllCategories);
 router.get('/category/:id', categoryController.getCategoryWithChildren);
 router.get('/:id', foodController.getFoodById);
+
+router.post('/category/add-sub',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.MANAGER, ENUM_USER_ROLE.SUPER_ADMIN),
+  categoryController.addSubCategory
+);
+router.patch('/category/update',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.MANAGER, ENUM_USER_ROLE.SUPER_ADMIN),
+  categoryController.updateCategoryWithSubs
+);
 
 
 
@@ -41,6 +58,8 @@ router.post('/addon/create',
   auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.MANAGER, ENUM_USER_ROLE.SUPER_ADMIN),
   addonController.createAddon
 );
+
+router.get('/addon/all', addonController.getAddOns);
 
 // router.patch('/foods/:foodId/status', foodController.updateStatus);
 // router.patch('/foods/:foodId/price', foodController.updatePrice);

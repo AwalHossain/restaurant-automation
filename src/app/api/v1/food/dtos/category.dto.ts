5ae3d22d08ebb0db814a97b4cea7c38e59e5d8e8
Awalhossain <waqin21@gmail.com>
2024-11-16 19:25:37 +0600

    feat: add AddSubCategoryInput and UpdateCategoryWithSubsInput types for enhanced category management

--- src/app/api/v1/food/dtos/category.dto.ts
+++ src/app/api/v1/food/dtos/category.dto.ts
@@ -7,3 +7,25 @@ export type CreateCategoryInput = {
     createdBy?: string;
     updatedBy?: string;
 }
+
+
+export type AddSubCategoryInput = {
+    mainCategoryId: string;
+    subCategory: string;
+    description?: string;
+    createdBy?: string;
+    updatedBy?: string;
+}
+
+export type UpdateCategoryInput = {
+    id: string;
+    name?: string;
+    description?: string;
+    isActive?: boolean;
+    updatedBy?: string;
+}
+
+export interface UpdateCategoryWithSubsInput {
+    mainCategory: UpdateCategoryInput;
+    subCategories?: UpdateCategoryInput[];
+}

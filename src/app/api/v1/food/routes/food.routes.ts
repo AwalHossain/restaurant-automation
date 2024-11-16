8b0364e72f96980d5baeaa2e26c1423ec960c774
Awalhossain <waqin21@gmail.com>
2024-11-16 10:13:03 +0600

    feat: add addon creation route with role-based authentication

--- src/app/api/v1/food/routes/food.routes.ts
+++ src/app/api/v1/food/routes/food.routes.ts
@@ -1,6 +1,9 @@
 // routes/food.routes.ts
 import { Router } from 'express';
 import multer from 'multer';
+import { ENUM_USER_ROLE } from '../../../../../enums/user';
+import auth from '../../../../middlewares/auth';
+import { AddonController } from '../controllers/addon.controller';
 import { CategoryController } from '../controllers/category.controller';
 import { FoodController } from '../controllers/food.controller';
 
@@ -14,6 +17,7 @@ const upload = multer({
 const router = Router();
 const foodController = new FoodController();
 const categoryController = new CategoryController();
+const addonController = new AddonController();
 
 router.post(
   '/create',
@@ -29,6 +33,14 @@ router.post('/category/create', categoryController.createCategory);
 router.get('/category/all', categoryController.getAllCategories);
 router.get('/category/:id', categoryController.getCategoryWithChildren);
 router.get('/:id', foodController.getFoodById);
+
+
+
+// Addon routes
+router.post('/addon/create',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.MANAGER, ENUM_USER_ROLE.SUPER_ADMIN),
+  addonController.createAddon
+);
 
 // router.patch('/foods/:foodId/status', foodController.updateStatus);
 // router.patch('/foods/:foodId/price', foodController.updatePrice);

ab597eb2865142899007db4ca3a1c1a85d9764f7
Awalhossain <waqin21@gmail.com>
2024-12-11 10:21:51 +0600

    refactor: rename addons to addonGroups in FoodService for improved clarity and consistency across service methods

--- src/app/api/v1/food/services/food.services.ts
+++ src/app/api/v1/food/services/food.services.ts
@@ -236,9 +236,9 @@ export class FoodService {
         categories: true,
         branches: true,
         campaign: true,
-        addons: {
+        addonGroups : {
           include: {
-            AddonGroup: {
+            addonGroup: {
               include: {
                 addons: {
                   include: {
@@ -295,9 +295,9 @@ export class FoodService {
         variants: true,
         categories: true,
         branches: true,
-        addons: {
+        addonGroups: {
           include: {
-            AddonGroup: true
+            addonGroup: true
           }
         },
         campaign: true
@@ -357,9 +357,9 @@ export class FoodService {
           }
         },
         branches: true,
-        addons: {
+        addonGroups: {
           include: {
-            AddonGroup: true
+            addonGroup: true
           }
         },
         campaign: true
@@ -440,9 +440,9 @@ export class FoodService {
           }
         },
         branches: true,
-        addons: {
+        addonGroups: {
           include: {
-            AddonGroup: {
+            addonGroup: {
               include: {
                 addons: {
                   include: {

2795d53c8926db00c621e3548bf07b79c00d46fc
Awalhossain <waqin21@gmail.com>
2024-12-11 08:14:25 +0600

    refactor: clean up FoodService class by removing commented-out code and improving formatting for better readability

--- src/app/api/v1/food/services/food.services.ts
+++ src/app/api/v1/food/services/food.services.ts
@@ -2,14 +2,12 @@ import { prisma } from "../../../../../shared/prisma";
 import { CreateFoodInput } from "../dtos/food.dto";
 import { FoodValidationService } from "../validation/food-validation.service";
 
-
-
 export class FoodService {
   private readonly foodValidationService: FoodValidationService;
-  constructor () {
+  constructor() {
     this.foodValidationService = new FoodValidationService();
   }
-  
+
   // async createFood(input: CreateFoodInput) {
   //   // Validate input
   //   await this.foodValidationService.validateCreateFoodInput(input);
@@ -24,7 +22,7 @@ export class FoodService {
   //         basePrice: input.basePrice,
   //         minOrderQuantity: input.minOrderQuantity,
   //         createdById: input.createdBy,
-          
+
   //         // Create food images
   //         foodImages: {
   //           create: input.images.map((image) => ({
@@ -81,7 +79,6 @@ export class FoodService {
   //   return createdFood;
   // }
 
-
   async createFood(input: CreateFoodInput) {
     const food = await prisma.food.create({
       data: {
@@ -90,7 +87,7 @@ export class FoodService {
         basePrice: input.basePrice,
         minOrderQuantity: input.minOrderQuantity,
         foodImages: {
-          create: input.images.map((image) => ({
+          create: input.images.map(image => ({
             url: image.url,
             deviceType: image.deviceType,
             width: image.width,
@@ -107,130 +104,148 @@ export class FoodService {
     return food;
   }
 
-
-
-    async updateFoodDetails(input: CreateFoodInput) {
-      
-      
-      const updatedFood = await prisma.food.update({
-        where: { id: input.id },
-        data: {
-          isPopular: input.isPopular,
-          isRecommended: input.isRecommended,
-          isNewArrival: input.isNewArrival,
-          freeDelivery: input.freeDelivery,
-          specialDeliveryFee: input.specialDeliveryFee,
-          haveDiscount: input.haveDiscount,
-          discountedPrice: input.discountedPrice,
-          offer: input.offer,
-          topSnacks: input.topSnacks,
-          dynamicHome: input.dynamicHome,
-          trending: input.trending,
-          isFree: input.isFree,
-          isFeatured: input.isFeatured,
-          expiryDate: input.expiryDate,
-          availableStartTime: input.availableStartTime,
-          availableEndTime: input.availableEndTime,
-          trendingStartTime: input.trendingStartTime,
-          trendingEndTime: input.trendingEndTime,
-          categories: {
-            connect: input?.categoryIds?.map(id => ({ id }))
-          },
-          basePrice: input.basePrice,
-          minOrderQuantity: input.minOrderQuantity,
-          updatedById: input.userId,
+  async updateFoodDetails(input: CreateFoodInput) {
+    const updatedFood = await prisma.food.update({
+      where: { id: input.id },
+      data: {
+        isPopular: input.isPopular,
+        isRecommended: input.isRecommended,
+        isNewArrival: input.isNewArrival,
+        freeDelivery: input.freeDelivery,
+        specialDeliveryFee: input.specialDeliveryFee,
+        haveDiscount: input.haveDiscount,
+        discountedPrice: input.discountedPrice,
+        offer: input.offer,
+        topSnacks: input.topSnacks,
+        dynamicHome: input.dynamicHome,
+        trending: input.trending,
+        isFree: input.isFree,
+        isFeatured: input.isFeatured,
+        expiryDate: input.expiryDate,
+        availableStartTime: input.availableStartTime,
+        availableEndTime: input.availableEndTime,
+        trendingStartTime: input.trendingStartTime,
+        trendingEndTime: input.trendingEndTime,
+        categories: {
+          connect: input?.categoryIds?.map(id => ({ id }))
         },
-        include: {
-          foodImages: true,
-          variants: true,
-          categories: true,
-          branches: true
+        basePrice: input.basePrice,
+        minOrderQuantity: input.minOrderQuantity,
+        updatedById: input.userId
+      },
+      include: {
+        foodImages: true,
+        variants: true,
+        categories: true,
+        branches: true
+      }
+    });
+    return updatedFood;
+  }
+
+  async getAllFoods() {
+    const foods = await prisma.food.findMany({
+      include: {
+        foodImages: true,
+        variants: true,
+        categories: true,
+        branches: true
+      }
+    });
+    return foods;
+  }
+
+  async getFoodById(id: string) {
+    const food = await prisma.food.findUnique({
+      where: { id },
+      include: {
+        foodImages: true,
+        variants: true,
+        categories: true,
+        branches: true,
+        campaign: true,
+        addons: {
+          include: {
+            AddonGroup: {
+              include: {
+                addons: true
+              }
+            }
+          }
         }
       }
-    );
-      return updatedFood;
-    }
-
-
-    async getAllFoods() {
-      const foods = await prisma.food.findMany({
-        include: {
-          foodImages: true,
-          variants: true,
-          categories: true,
-          branches: true
-        }
-      });
-      return foods;
-    } 
-
-    async getFoodById(id: string) {
-      const food = await prisma.food.findUnique({
-        where: { id },
-        include: {
-          foodImages: true,
-          variants: true,
-          categories: true,
-          branches: true,
-          campaign: true,
-          addons: true,
-        }
-      });
-      return food;
-    }
+    });
+    return food;
+  }
 
   async getFoodByMainCategoryId(id: string) {
-      const food = await prisma.food.findMany({
-        where: { categories:{
-          some: { 
-            OR:[
-              {id},
-              {parentId: id}
-            ]
+    const food = await prisma.food.findMany({
+      where: {
+        categories: {
+          some: {
+            OR: [{ id }, { parentId: id }]
           }
-        } },
-        include: {
-          foodImages: true,
-          variants: true,
-          categories: true,
-          branches: true,
-          addons:{
-            include:{
-              addon: true
-            }
-          },
-          campaign: true
         }
-      });
-      return food;
-    }
-
-    async getFoodBySubCategoryId(id: string) {
-      const food = await prisma.food.findMany({
-            where: { categories:{
-              some: { 
-                id,
-                 // Ensure it's actually a subcategory
-                parentId:{
-                  not: null
-                },
-                isActive: true
-               },
-            }},
-            include: {
-              foodImages: true,
-              variants: true,
-              categories: true,
-              branches: true,
-              addons:{
-                include:{
-                  addon: true
+      },
+      include: {
+        foodImages: true,
+        variants: true,
+        categories: true,
+        branches: true,
+        addons: {
+          include: {
+            AddonGroup: {
+              include: {
+                addons: {
+                  include: {
+                    addon: true
+                  }
                 }
-              },
-              campaign: true
+              }
             }
-      });
-      return food;
-    }
-    
+          }
+        },
+        campaign: true
+      }
+    });
+    return food;
+  }
+
+  async getFoodBySubCategoryId(id: string) {
+    const food = await prisma.food.findMany({
+      where: {
+        categories: {
+          some: {
+            id,
+            // Ensure it's actually a subcategory
+            parentId: {
+              not: null
+            },
+            isActive: true
+          }
+        }
+      },
+      include: {
+        foodImages: true,
+        variants: true,
+        categories: true,
+        branches: true,
+        addons: {
+          include: {
+            AddonGroup: {
+              include: {
+                addons: {
+                  include: {
+                    addon: true
+                  }
+                }
+              }
+            }
+          }
+        },
+        campaign: true
+      }
+    });
+    return food;
+  }
 }

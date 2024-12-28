c4c25307c01c15e6e4fd0ba74a0810959d31192a
Awalhossain <waqin21@gmail.com>
2024-12-28 22:06:03 +0600

    feat: implement BranchController with CRUD operations for branches; add methods for creating, updating, retrieving, and deleting branches, including business hours management and user authentication checks

---
+++ src/app/api/v1/branch/controller/branch.controller.ts
@@ -0,0 +1,123 @@
+import { Request, Response } from "express";
+import httpStatus from "http-status";
+import ApiError from "../../../../../errors/ApiError";
+import catchAsync from "../../../../../shared/catchAsync";
+import sendResponse from "../../../../../shared/sendResponse";
+import { BranchService } from "../services/banch.services";
+import { BranchValidationService } from "../validation/branch.validation";
+
+
+export class BranchController {
+    private branchService: BranchService  
+    constructor(
+    ) {
+      this.branchService = new BranchService(new BranchValidationService());
+    }
+
+    createBranch = catchAsync(async (req: Request, res: Response) => {
+      const userId = req.user?.userId;
+      if (!userId) {
+        throw new ApiError(400, 'User not found');
+      }
+      const result = await this.branchService.createBranch(req.body, userId, req);
+
+      sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Branch created successfully",
+        data: result
+      })
+    })
+
+    updateBranchBusinessHours = catchAsync(async (req: Request, res: Response) => {
+      const userId = req.user?.userId;
+      if (!userId) {
+        throw new ApiError(400, 'User not found');
+        
+      }
+
+      const inputData = {
+        branchId: req.params.branchId,
+        businessHours: req.body.businessHours,
+        userId: userId,
+        req: req
+      }
+      const result = await this.branchService.updateBranchBusinessHours(inputData);
+
+      sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Branch business hours updated successfully",
+        data: result
+      })
+    })
+
+    getAllBranch = catchAsync(async (req: Request, res: Response) => {
+      const result = await this.branchService.getAllBranch();
+
+      sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Branch fetched successfully",
+        data: result
+      })
+    })
+
+    getBranchById = catchAsync(async (req: Request, res: Response) => {
+      const result = await this.branchService.getBranchById(req.params.branchId);
+
+      sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Branch fetched successfully",
+        data: result
+      })
+    })
+
+    updateBranchStatus = catchAsync(async (req: Request, res: Response) => {
+      const userId = req.user?.userId;
+      if (!userId) {
+        throw new ApiError(400, 'User not found');
+      }
+
+      const inputData = {
+        branchId: req.params.branchId,
+        userId: userId,
+        req: req
+      }
+      const result = await this.branchService.updateBranchStatus(inputData);
+
+      sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Branch status updated successfully",
+        data: result
+      })
+    })
+
+    deleteBranch = catchAsync(async (req: Request, res: Response) => {
+      const userId = req.user?.userId;  
+      if (!userId) {
+        throw new ApiError(400, 'User not found');
+      }
+      const result = await this.branchService.deleteBranch(req.params.branchId, userId);
+  
+      sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Branch deleted successfully",
+        data: result
+      })
+    })
+
+    getAllActiveBranch = catchAsync(async (req: Request, res: Response) => {
+      const result = await this.branchService.getAllActiveBranch();
+
+      sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Branch fetched successfully",
+        data: result
+      })
+    })
+}

078aaa9e4a965081e6cc56051540293a6b2e12d0
Awalhossain <waqin21@gmail.com>
2025-01-11 19:38:25 +0600

    fix: update import path for auth middleware in addon routes to improve module resolution

--- src/app/api/v1/food/routes/addon.routes.ts
+++ src/app/api/v1/food/routes/addon.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import multer from "multer";
 import { ENUM_USER_ROLE } from "../../../../../enums/user";
-import auth from "../../../../middlewares/auth";
+import auth from "../../../../middlewares/auth/auth-middleware";
 import { ControllerFactory } from "../factories/controller.factory";
 
 const addonController = ControllerFactory.createAddonController();

c18f761c2c7a40446a27cccb0b3b48e3758e34a5
Awalhossain <waqin21@gmail.com>
2025-01-11 19:41:18 +0600

    feat: implement getPermissions function to define role-based access control for user permissions

---
+++ src/helpers/getPermission.ts
@@ -0,0 +1,51 @@
+import { Role } from "@prisma/client"
+
+export const getPermissions = (role: Role) => {
+
+    const basePermissions ={
+        canViewOrders: false,
+        canManageStaff: false,
+        canManageInventory: false,
+        canViewReports: false,
+        canManageSettings: false,
+        canViewCustomers: false,
+        canViewProducts: false,
+        canViewBranches: false,
+        canViewStaff: false,
+        canViewSettings: false,
+    }
+
+    switch(role){
+        case Role.MANAGER:
+            return {
+                ...basePermissions,
+                canManageStaff: true,
+                canManageInventory: true,
+                canViewReports: true,
+                canManageSettings: true,
+                canViewCustomers: true,
+                canViewProducts: true,
+                canViewBranches: true,
+                canViewStaff: true,
+                canViewSettings: true,
+            }
+        case Role.MODERATOR:
+            return {
+                ...basePermissions,
+                canViewOrders: true,
+                canManageInventory: true,
+                canViewReports: true,
+                canViewCustomers: true,
+                canViewProducts: true,
+            }
+        case Role.RIDER:
+            return {
+                ...basePermissions,
+                canViewOrders: true,
+                canViewCustomers: true,
+                canViewProducts: true,
+            }
+        default:
+            return basePermissions
+    }
+}

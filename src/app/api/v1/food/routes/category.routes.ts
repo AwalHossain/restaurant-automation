a1569bff518fea1e4de8a6fda495d091a329b30e
Awalhossain <waqin21@gmail.com>
2025-01-11 19:38:42 +0600

    fix: update import path for auth middleware in category routes to improve module resolution

--- src/app/api/v1/food/routes/category.routes.ts
+++ src/app/api/v1/food/routes/category.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { ENUM_USER_ROLE } from "../../../../../enums/user";
 import { userContextMiddleware } from "../../../../../middlewares/user-context.middleware";
-import auth from "../../../../middlewares/auth";
+import auth from "../../../../middlewares/auth/auth-middleware";
 import { ControllerFactory } from "../factories/controller.factory";
 
 const router = Router();

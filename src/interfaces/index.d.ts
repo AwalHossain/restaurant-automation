e178c52f47bbb421933139fdb47074e6fcbb11ab
Awalhossain <waqin21@gmail.com>
2025-01-11 19:41:28 +0600

    feat: extend Express Request interface to include optional branchRole for enhanced role management

--- src/interfaces/index.d.ts
+++ src/interfaces/index.d.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
+import { Role } from '@prisma/client';
 import { JwtPayload } from 'jsonwebtoken';
 
 declare global {
   namespace Express {
     interface Request {
       user: JwtPayload | null;
+      branchRole?: Role;
     }
   }
 }

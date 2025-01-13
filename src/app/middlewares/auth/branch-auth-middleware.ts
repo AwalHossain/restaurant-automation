b2bf613fcff4c0da0bc502b8827a94b8497f4bab
Awalhossain <waqin21@gmail.com>
2025-01-13 11:26:04 +0600

    feat: enhance branch authentication middleware to include JWT verification, improved error handling for authorization, and expanded role checks for ADMIN access

--- src/app/middlewares/auth/branch-auth-middleware.ts
+++ src/app/middlewares/auth/branch-auth-middleware.ts
@@ -1,7 +1,10 @@
 import { Role } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
+import { JwtPayload, Secret } from "jsonwebtoken";
+import env from "../../../config";
 import ApiError from "../../../errors/ApiError";
+import { jwtHelpers } from "../../../helpers/jwtHelpers";
 import { prisma } from "../../../shared/prisma";
 
 
@@ -9,8 +12,35 @@ import { prisma } from "../../../shared/prisma";
 export const branchAuth = (allowedRoles: Role[]) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         try{
-                const {branchId} = req.body || req.params;
+
+                //get authorization token
+                const authHeader = req.headers.authorization;
+                if (!authHeader) {
+                  throw new ApiError(httpStatus.UNAUTHORIZED, 'You are not authorized');
+                }
+          
+                // Extract token from "Bearer <token>"
+                const token = authHeader.startsWith('Bearer ')
+                  ? authHeader.substring(7) // Remove "Bearer " prefix
+                  : authHeader;
+          
+                console.log(token, 'token, token');
+          
+                if (!token) {
+                  throw new ApiError(httpStatus.UNAUTHORIZED, 'Invalid token format');
+                }
+                // verify token
+                let verifiedUser = null;
+          
+                verifiedUser = jwtHelpers.verifyToken(token, env.JWT_SECRET as Secret);
+                console.log(verifiedUser, 'verifiedUser');
+                req.user = verifiedUser as JwtPayload; // role  , userid
+                console.log(req.user, 'req.user', verifiedUser); 
+            console.log(req.params,'req.params');
+                const branchId = req.body.branchId || req.params.branchId;
                 const userId = req.user?.userId;
+                console.log(branchId,'branchId');
+                console.log(userId,'userId');
                 if(!branchId || !userId) {
                     throw new ApiError(httpStatus.UNAUTHORIZED, 'Branch ID and User ID are required');
                 }
@@ -36,9 +66,11 @@ export const branchAuth = (allowedRoles: Role[]) => {
                 }
 
                 // Super admin can access all branches
-                if(user.role === Role.SUPER_ADMIN) {
+                if(user.role === Role.SUPER_ADMIN || user.role === Role.ADMIN) {
                     return next();
-                }
+                }   
+
+                console.log(user.branchStaff[0],'user.branchStaff[0]', user.branchStaff, 'user.branchStaff');
 
                 // check if the user has the required role for the branch
                 const branchStaff = user.branchStaff[0];

095d659cc5d3bccbce8746960975a5db98bce787
Awalhossain <waqin21@gmail.com>
2025-01-23 21:13:01 +0600

    feat: integrate tenant context middleware in category creation route to enhance multi-tenancy support for food categories

--- src/app/api/v1/food/routes/category.routes.ts
+++ src/app/api/v1/food/routes/category.routes.ts
@@ -2,13 +2,17 @@ import { Router } from "express";
 import { ENUM_USER_ROLE } from "../../../../../enums/user";
 import { userContextMiddleware } from "../../../../../middlewares/user-context.middleware";
 import auth from "../../../../middlewares/auth/auth-middleware";
+import tenantContextMiddleware from "../../../../middlewares/auth/tenant-context.middleware";
 import { ControllerFactory } from "../factories/controller.factory";
 
 const router = Router();
 
 const categoryController = ControllerFactory.createCategoryController();
 
-router.post("/create", categoryController.createCategory);
+router.post("/create",
+    auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.MANAGER, ENUM_USER_ROLE.SUPER_ADMIN),
+    tenantContextMiddleware(),
+   categoryController.createCategory);
 router.get("/all", categoryController.getAllCategories);
 router.get("/active", categoryController.getActiveCategories);
 router.get("/:id", categoryController.getCategoryWithChildren);

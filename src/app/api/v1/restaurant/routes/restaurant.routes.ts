ff08858db89c2c51e0d20eecb783f19e111a8660
Awalhossain <waqin21@gmail.com>
2025-01-25 14:21:40 +0600

    feat: update restaurant routes with tenant context middleware and new endpoints

--- src/app/api/v1/restaurant/routes/restaurant.routes.ts
+++ src/app/api/v1/restaurant/routes/restaurant.routes.ts
@@ -1,6 +1,7 @@
 import { Role } from "@prisma/client";
 import { Router } from "express";
 import auth from "../../../../middlewares/auth/auth-middleware";
+import tenantContextMiddleware from "../../../../middlewares/auth/tenant-context.middleware";
 import { RestaurantController } from "../controller/restaurant.controller";
 import { RestaurantService } from "../services/restaurant.service";
 import { RestaurantValidationService } from "../validations/restaurant.validation";
@@ -11,19 +12,53 @@ const restaurantController = new RestaurantController(
   new RestaurantValidationService()
 );
 
+// Create Restaurant
 router.post("/create",
   auth(Role.ADMIN, Role.SUPER_ADMIN),
    restaurantController.createRestaurant);
+
+  // Get restaurant with branch and settings
 router.get("/",
   auth(Role.SUPER_ADMIN, Role.ADMIN),
+  tenantContextMiddleware(),
   restaurantController.getAllRestaurants);
+
+  // Restaurant By Domain
 router.get("/:domain/domain",
-  auth(Role.ADMIN, Role.SUPER_ADMIN),
+  // auth(Role.ADMIN, Role.SUPER_ADMIN),
+  // tenantContextMiddleware(),
   restaurantController.getRestaurantByDomain);
+
+  // Restaurant Settings
 router.patch("/:restaurantId/update-settings",
   auth(Role.ADMIN, Role.SUPER_ADMIN),
+  tenantContextMiddleware(),
   restaurantController.updateRestaurantSettings);
+
+  // Restaurant By Admin Id
+router.get("/:adminId/admin",
+  auth(Role.ADMIN, Role.SUPER_ADMIN),
+  tenantContextMiddleware(),
+  restaurantController.getRestaurantByAdminId);
+
+  // Restaurant By Tenant Id
+router.get("/:tenantId/tenant",
+  auth(Role.ADMIN, Role.SUPER_ADMIN),
+  tenantContextMiddleware(),
+  restaurantController.getRestaurantByTenantId);
+
+
+  // Points System
 router.patch("/:restaurantId/update-points-system",
   auth(Role.ADMIN, Role.SUPER_ADMIN),
+  tenantContextMiddleware(),
   restaurantController.updatePointsSystem);
+
+// get all branches related to restaurant
+router.get("/:restaurantId/branches",
+  auth(Role.ADMIN, Role.SUPER_ADMIN),
+  tenantContextMiddleware(),
+  restaurantController.getAllBranches);
+
+  
 export const RestaurantRoutes = router;

62638504ade7beb5285422e5a09f55f43622aaf9
Awalhossain <waqin21@gmail.com>
2025-01-25 14:14:12 +0600

    feat: add tenant context middleware to staff authentication routes

--- src/app/api/v1/authentication/routes/auth.routes.ts
+++ src/app/api/v1/authentication/routes/auth.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import tenantContextMiddleware from '../../../../middlewares/auth/tenant-context.middleware';
 import { AuthController } from '../controllers/auth.controller';
 
 const router = Router();
@@ -10,9 +11,22 @@ router.post('/register-user', controller.completeUserRegistration);
 router.post('/login-user', controller.userLogin);
 
 
+// admin routes
+router.post('/register', controller.superAdminRegister);
+router.post('/login',
+    controller.superAdminLogin);
+
+// super admin routes
+router.post('/super/register', controller.superAdminRegister);
+router.post('/super/login', controller.superAdminLogin);
+
 // staff/Admin routes (for internal use only)
-router.post('/staff/register', controller.staffRegister);
-router.post('/staff/login', controller.staffLogin);
+router.post('/staff/register', 
+    tenantContextMiddleware(),
+    controller.staffRegister);
+router.post('/staff/login', 
+    tenantContextMiddleware(),
+    controller.staffLogin);
 
 
 export const AdminAuthRoutes = router;

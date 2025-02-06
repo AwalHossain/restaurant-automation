145682b6ab23fbde2f9a86b546dac2251704a442
Awalhossain <waqin21@gmail.com>
2025-02-07 00:41:26 +0600

    feat: enhance auth routes with role selection, switching, and user context endpoints

--- src/app/api/v1/authentication/routes/auth.routes.ts
+++ src/app/api/v1/authentication/routes/auth.routes.ts
@@ -1,11 +1,15 @@
+import { Role } from '@prisma/client';
 import { Router } from 'express';
+import { RestaurantPermissionNames } from '../../../../../types/permission.types';
+import auth from '../../../../middlewares/auth/auth-middleware';
+import { accessControl } from '../../../../middlewares/auth/permission-middleware';
 import tenantContextMiddleware from '../../../../middlewares/auth/tenant-context.middleware';
 import { AuthController } from '../controllers/auth.controller';
 
 const router = Router();
 const controller = new AuthController();
 
-router.post('/send-otp',  controller.initiateUserRegistration);
+router.post('/send-otp', controller.initiateUserRegistration);
 router.post('/verify-otp', controller.verifyOTP);
 router.post('/register-user', controller.completeUserRegistration);
 router.post('/login-user', controller.userLogin);
@@ -13,19 +17,43 @@ router.post('/login-user', controller.userLogin);
 
 // admin routes
 router.post('/register', controller.adminRegister);
-router.post('/login', controller.adminLogin);
+router.post('/login', controller.unifiedLogin);
 
 // super admin routes
-router.post('/super/register', controller.superAdminRegister);
-router.post('/super/login', controller.superAdminLogin);
+// router.post('/super/register', controller.superAdminRegister);
+router.post('/super/login', controller.unifiedLogin);
 
 // staff/Admin routes (for internal use only)
-router.post('/staff/register', 
+router.post('/staff/register',
+    auth(),
     tenantContextMiddleware(),
-    controller.staffRegister);
-router.post('/staff/login', 
+    accessControl({
+        allowedRoles: [Role.SUPER_ADMIN, Role.ADMIN],
+        staffPermissions: [
+            RestaurantPermissionNames.MANAGE_RESTAURANT_USERS
+        ]
+    }),controller.staffRegister);
+
+
+router.post('/staff/login',
     tenantContextMiddleware(),
-    controller.staffLogin);
+    controller.unifiedLogin);
+
+// auth.routes.ts
+router.get('/staff/select-role',
+    auth(),
+    tenantContextMiddleware(),
+    controller.selectRole);
+
+router.get('/staff/switch-role',
+    auth(),
+    tenantContextMiddleware(),
+    controller.switchRole);
+
+router.get('/staff/user-context',
+    auth(),
+    tenantContextMiddleware(),
+    controller.getUserContext);
 
 
 export const AdminAuthRoutes = router;

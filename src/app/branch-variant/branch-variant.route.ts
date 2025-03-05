5f79e6115444b851f3b603e68065ef5d23754c7b
Awalhossain <waqin21@gmail.com>
2025-03-05 15:30:28 +0600

    brancch permission

--- src/app/branch-variant/branch-variant.route.ts
+++ src/app/branch-variant/branch-variant.route.ts
@@ -1,23 +1,36 @@
 import { Router } from "express";
-import { ENUM_USER_ROLE } from "../../enums/user";
+import { BranchPermissionNames as BPN, RestaurantPermissionNames as RPN, RRole } from "../../types/permission.types";
 import auth from "../middlewares/auth/auth-middleware";
-import tenantContextMiddleware from "../middlewares/auth/tenant-context.middleware";
+import branchTenantContextMiddleware from "../middlewares/auth/branch-tenantContext-middleware";
+import { accessControl } from "../middlewares/auth/permission-middleware";
 import { BranchVariantController } from "./branch-variant.controller";
 
 const branchVariantController = new BranchVariantController();
 
 const router = Router();
 router.post("/create/:foodId", 
-    auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
-    tenantContextMiddleware(),
+    auth(),
+  branchTenantContextMiddleware(),
+  accessControl({
+    allowedRoles:[RRole.RESTAURANT_ADMIN, RRole.BRANCH_MANAGER],
+    staffPermissions:[BPN.MANAGE_BRANCH_ADDON, RPN.MANAGE_RESTAURANT_BRANCHES]
+  }),
     branchVariantController.bulkUpdateVariants);
 router.get("/all/:foodId",
-    auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
-    tenantContextMiddleware(),
+    auth(),
+  branchTenantContextMiddleware(),
+  accessControl({
+    allowedRoles:[RRole.RESTAURANT_ADMIN, RRole.BRANCH_MANAGER],
+    staffPermissions:[BPN.MANAGE_BRANCH_ADDON, RPN.MANAGE_RESTAURANT_BRANCHES]
+  }),
     branchVariantController.getVariantsByFoodId);
 router.patch("/update/:foodId/food-variant",
-    auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
-    tenantContextMiddleware(),
+    auth(),
+  branchTenantContextMiddleware(),
+  accessControl({
+    allowedRoles:[RRole.RESTAURANT_ADMIN, RRole.BRANCH_MANAGER],
+    staffPermissions:[BPN.MANAGE_BRANCH_ADDON, RPN.MANAGE_RESTAURANT_BRANCHES]
+  }),
     branchVariantController.updateVariant);
 
 export const VariantRoutes = router;

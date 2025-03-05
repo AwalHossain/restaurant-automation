ebc0316ff20d36638929417e23add48a7cb363cd
Awalhossain <waqin21@gmail.com>
2025-03-05 15:28:16 +0600

    refactor(role.dto.ts): update permissionNames type to PermissionName[]

--- src/app/api/v1/role-permission/dtos/role.dto.ts
+++ src/app/api/v1/role-permission/dtos/role.dto.ts
@@ -1,14 +1,19 @@
 import { RoleScope } from "@prisma/client";
-
+import { PermissionName } from "../../../../../types/permission.types";
 
 
 export interface CreateRoleDto {
     name: string;
     tenantId: string;
     description: string;
-    permissionNames: string[];
+    permissionNames: PermissionName[];
     inheritedFromId?: string;
     scope: RoleScope;
+
+
+
+
+
 }
 
 export interface UpdateRoleDto extends Partial<CreateRoleDto> {}

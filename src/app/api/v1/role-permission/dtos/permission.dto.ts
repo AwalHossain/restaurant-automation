74ee1a08512661fd5420b80ca97b31475e80c634
Awalhossain <waqin21@gmail.com>
2025-03-05 15:29:32 +0600

    brancch permission validation

--- src/app/api/v1/role-permission/dtos/permission.dto.ts
+++ src/app/api/v1/role-permission/dtos/permission.dto.ts
@@ -4,6 +4,23 @@ export interface CreatePermissionDto {
     tenantId: string;
 }
 
+export interface UserRoles {
+    id: string;
+    username: string;
+    email: string;
+    phone: string;
+    allPermissions: string[];
+    restaurantRoles: {
+
+      restaurantId: string;
+      roleName: string;
+    }[];
+    branchRoles: {
+      branchId: string;
+      roleName: string;
+    }[];
+  }
+
 export interface UpdatePermissionDto extends Partial<CreatePermissionDto> {
     updatedById?: string;
 } 
